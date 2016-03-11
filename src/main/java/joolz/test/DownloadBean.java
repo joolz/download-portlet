441f47103af80be2b9407fdaa3a3da43a127e844
joolz <joolz@xs4all.nl>
2016-03-11 09:06:07 +0100

    Poller functionality

--- src/main/java/joolz/test/DownloadBean.java
+++ src/main/java/joolz/test/DownloadBean.java
@@ -11,7 +11,6 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 
-import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
@@ -25,17 +24,35 @@ public class DownloadBean {
 	private static final String TEST_FILE = "download-test-";
 	private File tempFile;
 	private StreamedContent file = null;
+	private boolean prepared = false;
+	private static final int FIVE_SECONDS = 1000 * 5;
+	private boolean stopPoll = false;
 
-	@PostConstruct
-	public void init() {
+	public void doPrepare() {
 		try {
 			tempFile = File.createTempFile(TEST_FILE, null);
 			tempFile.deleteOnExit();
 			LOG.debug("Created tempfile " + tempFile.getCanonicalPath());
-			Files.write(Paths.get(tempFile.getCanonicalPath()), getTestContent().getBytes(), StandardOpenOption.APPEND);
-		} catch (final IOException e) {
+			final StringBuilder sb = new StringBuilder();
+			sb.append("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
+			sb.append("\n");
+			sb.append("Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?");
+			sb.append("\n\n");
+			for (int i = 0; i < 2; i++) {
+				final String append = i + "\n" + sb.toString();
+				Files.write(Paths.get(tempFile.getCanonicalPath()), append.getBytes(), StandardOpenOption.APPEND);
+				LOG.debug("Appended " + i + ", now sleep " + FIVE_SECONDS + " milliseconds");
+				Thread.sleep(FIVE_SECONDS);
+			}
+			LOG.debug("Preparation finished");
+			prepared = true;
+		} catch (final IOException | InterruptedException e) {
 			LOG.error(e);
 		}
+	}
+
+	public boolean isPrepared() {
+		return prepared;
 	}
 
 	public StreamedContent getFile() {
@@ -43,21 +60,23 @@ public class DownloadBean {
 			LOG.debug("file is null, get it");
 			try {
 				final InputStream stream = new FileInputStream(tempFile);
-				file = new DefaultStreamedContent(stream, "text/plain", tempFile.getName());
+				file = new DefaultStreamedContent(stream, "text/plain", "MyFile.txt");
 			} catch (final IOException e) {
 				LOG.error(e);
 			}
-
 		}
 		return file;
 	}
 
-	private String getTestContent() {
-		final StringBuilder sb = new StringBuilder();
-		sb.append("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
-		sb.append("\n");
-		sb.append("Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?");
-		return sb.toString();
+	public void poller() {
+		LOG.debug("poller called, prepared is " + prepared);
+		if (prepared) {
+			stopPoll = true;
+		}
+	}
+
+	public boolean isStopPoll() {
+		return stopPoll;
 	}
 
 }
